926431e1ac766f6f676e4a081457f869536586e2
Elena <Elena@PINGBIN>
2016-10-30 12:58:42 +0300

    Добавьте файлы проекта.

---
+++ GSU-EPAM_Autumn_2016/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSU_EPAM_Autumn_2016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
