16b32cd4e1a199fc94284857fb5a98cf7610657c
Taler <mrtaler@me.com>
2016-11-03 20:19:26 +0300

    Task01 is done

--- GSU-EPAM_Autumn_2016/Task01/Task1.cs
+++ GSU-EPAM_Autumn_2016/Task01/Task1.cs
@@ -23,10 +23,9 @@ namespace GSU_EPAM_Autumn_2016.Task01
             //read all files
             foreach (var itemFileName in fileNameaArray)
             {
-
                 Console.WriteLine("File is: " + itemFileName); //show file name with worked now
                 string[] war = LoadFileInStringPerLineStrings(itemFileName); //read all file per line
-                StringBuilder[] result = Task1(war); //work with file data in string
+                StringBuilder[] result = CountSummFromAllString(war); //work with file data in string
 
                 //show result
                 foreach (var itemRes in result)
@@ -52,7 +51,7 @@ namespace GSU_EPAM_Autumn_2016.Task01
             string substr = "bin\\debug";
             string filePatch = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)
                  .ToLower().Replace(substr, "")
-                 + "Task1\\" + fileName;
+                 + "Task01\\" + fileName;
 
             string[] fileDataPerLine = null;
             #region read and write not null stribg from file
@@ -76,17 +75,14 @@ namespace GSU_EPAM_Autumn_2016.Task01
                         }
                     }
                 }
-                catch (Exception exception)
+                catch (Exception)
                 {
-                    Console.WriteLine("s: " +
-                        exception);
+                    Console.WriteLine("s: File is not open for read");
                 }
                 #endregion
                 //generate string based on conut not null strings in file
                 fileDataPerLine = new string[count];
                 #region write all not null string from file
-
-
                 try
                 {
                     using (StreamReader ins = new StreamReader(filePatch))
@@ -104,28 +100,18 @@ namespace GSU_EPAM_Autumn_2016.Task01
                         }
                     }
                 }
-                catch (Exception exception)
+                catch (Exception)
                 {
-                    Console.WriteLine("s: " +
-                exception);
-                    fileDataPerLine = new string[]
-                    {
-                        "fail"
-                    };
-
+                    Console.WriteLine("s: File is not open for read and write to file");
+                    fileDataPerLine = null;
                 }
                 #endregion
                 return fileDataPerLine;
             }
-            catch (Exception exception)
+            catch (Exception)
             {
-                fileDataPerLine = new string[]
-                    {
-                         "fail"
-                    };
-                Console.WriteLine("s: " +
-                    exception);
-                return fileDataPerLine;
+                Console.WriteLine("s: File is not open");
+                return null;
             }
             #endregion
         }
@@ -140,54 +126,66 @@ namespace GSU_EPAM_Autumn_2016.Task01
             double sumResult = 0;
             int stringErrorCount = 0;
             StringBuilder resultString = new StringBuilder();
-            foreach (var item in fileDataPerLineinString)
+            try
             {
-                int indexToWork = 0;
-                string[] format = item.Split(';');//
-                try
+                foreach (var item in fileDataPerLineinString)
                 {
-                    indexToWork = Convert.ToInt32(format[0]);
+                    int indexToWork = 0;
+                    string[] format = item.Split(';');//CSV Separator
                     try
                     {
-                        //  Console.WriteLine(double.Parse(format[indexToWork].ToString()));
-                        sumResult += double.Parse(format[indexToWork].ToString());
-                        if (resultString.Length == 0)
+                        indexToWork = Convert.ToInt32(format[0]); //find index
+                                                                  //if indexToWork is valid:
+                        try
                         {
-                            resultString.Append("result(" + double.Parse(format[indexToWork].ToString()));
+                            //  Console.WriteLine(double.Parse(format[indexToWork].ToString()));
+
+                            sumResult += double.Parse(format[indexToWork]);//adding valid finded number to sum result
+                            #region output string builder
+                            if (resultString.Length == 0)
+                            {
+                                resultString.Append("result(" + double.Parse(format[indexToWork].ToString()));
+                            }
+                            else if (double.Parse(format[indexToWork]) > 0)
+                            {
+                                resultString.Append(" + " + double.Parse(format[indexToWork].ToString()));
+                            }
+                            else if (double.Parse(format[indexToWork]) < 0)
+                            {
+                                resultString.Append(" - " + Math.Abs(double.Parse(format[indexToWork])).ToString());
+                            }
+                            else if (Math.Abs(double.Parse(format[indexToWork])) < 1e-19)
+                            {
+                                resultString.Append(" + 0.0");
+                            }
+                            #endregion
                         }
-                        else if (double.Parse(format[indexToWork]) > 0)
+                        catch (Exception)
                         {
-                            resultString.Append(" + " + double.Parse(format[indexToWork].ToString()));
-                        }
-                        else if (double.Parse(format[indexToWork]) < 0)
-                        {
-                            resultString.Append(" - " + Math.Abs(double.Parse(format[indexToWork])).ToString());
-                        }
-                        else if (double.Parse(format[indexToWork]) == 0)
-                        {
-                            resultString.Append(" + 0.0");
+                            //  Console.WriteLine("symbol is valid");
+                            stringErrorCount++;
+                            //output strib builder
+                            if (resultString.Length == 0)
+                                resultString.Append("result(");
                         }
                     }
                     catch (Exception)
                     {
-                        //  Console.WriteLine("sumbol is not number");
+                        //Console.WriteLine("First symbol is not number");
                         stringErrorCount++;
-                        if (resultString.Length == 0)
-                            resultString.Append("result(");
                     }
                 }
-                catch (Exception)
-                {
-                    //Console.WriteLine("First sumbol is not number");
-                    stringErrorCount++;
-                }
+            }
+            catch (Exception)
+            {
 
-
+                Console.WriteLine("input data is null");
+                resultString.Append("result(");
             }
             return (new StringBuilder[2]
                {
-                   resultString.Append(") = " + Math.Round(sumResult,2)),
-                 new  StringBuilder("error-lines = "+stringErrorCount)
+                 resultString.Append(") = " + Math.Round(sumResult,2)), //first returned summ value in special format
+                 new  StringBuilder("error-lines = "+stringErrorCount)  //second returned count error on special format
                });
         }
     }
--- GSU-EPAM_Autumn_2016/Task02/Task2.cs
+++ GSU-EPAM_Autumn_2016/Task02/Task2.cs
@@ -9,33 +9,6 @@ namespace GSU_EPAM_Autumn_2016.Task02
     {
         public Task2()
         {
-            Console.WriteLine("----------------------------------1----------------------------------");
-            AbstractPurchase[] pofg = new AbstractPurchase[] 
-            {
-                new PurchaseWithDiscountToCostAbstractPurchase("T1",6.85M,15,1.35M),
-                new PurchaseWithDiscountToCostAbstractPurchase("T2",7.85M,17,2.05M),
-                new PurchaseWithDiscountToCountAbstractPurchase("T3",9.54M,43,12),
-                new PurchaseWithDiscountToCountAbstractPurchase("T4",8.54M,21,5),
-                new PurchaseWithTransportationCostsAbstractPurchase("T5",3.53M,4,21),
-                new PurchaseWithTransportationCostsAbstractPurchase("T6",1.34M,14,2)
-            };
-            Console.WriteLine("----------------------------------2----------------------------------");
-            print(pofg);
-
-
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("----------------------------------3----------------------------------");
-            Array.Sort(pofg);
-            Console.WriteLine("----------------------------------4----------------------------------");
-            print(pofg);
-        }
-        private void print(AbstractPurchase[] printList)
-        {
-            foreach (var item in printList)
-            {
-                Console.WriteLine(item);
-            }
         }
     }
 }
