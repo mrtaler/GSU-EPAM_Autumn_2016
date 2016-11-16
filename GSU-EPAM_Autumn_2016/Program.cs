8d68b8435f8057da1d102dbfdb8d83faf424b91e
Taler <mrtaler@me.com>
2016-11-16 19:27:07 +0300

    up

--- GSU-EPAM_Autumn_2016/Program.cs
+++ GSU-EPAM_Autumn_2016/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using GSU_EPAM_Autumn_2016.Task01;
-using GSU_EPAM_Autumn_2016.Task02;
 using System.IO;
-using GSU_EPAM_Autumn_2016.Task03;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace GSU_EPAM_Autumn_2016
 {
@@ -10,14 +10,64 @@ namespace GSU_EPAM_Autumn_2016
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("*******************Task 01*******************");
-            new Task1();
-            Console.WriteLine("*******************Task 02*******************");
-            new Task2();
-            Console.WriteLine("*******************Task 03*******************");
-            new Task3();
-            Console.WriteLine("*********************end*********************");
-            Console.ReadKey();
+            {
+                string fileNameToRead = "input.txt";
+                string fileNameToWrite = "Output.txt";
+
+
+
+
+
+                string commentPattern1 = @"(?<=[;]| )(([\/]{2,}.+)|\/\/)";// for // comment
+                                                                          //
+                                                                          //  string commentPattern2 = @"([\/][\*](\w|\P{L})*?[\*][\/])";
+                string commentPattern3 = @"[""].*?[""]|([\/][\*](\w|[^;])*?[\*][\/])";//[""].*?[""]|([\/][\*](\w|[^;])*?[\*][\/])
+
+                string[] stringFromFile = ReadAndWriteTofilePerString.LoadFileInStringPerLineStrings(fileNameToRead); //read all file per line in array
+                #region UNION all strigs in 1 mega string
+                StringBuilder strResult = new StringBuilder();
+
+                foreach (var VARIABLE in stringFromFile)
+                {
+                    strResult.Append(VARIABLE);
+                    strResult.Append('\n');
+                }
+                #endregion
+
+                string resultFromStringBuilder = "";
+                #region Delete all end string comment, after ;//
+                if (Regex.IsMatch(strResult.ToString(), commentPattern1))
+                {
+                    resultFromStringBuilder = Regex.Replace(strResult.ToString(), commentPattern1, "");
+                    var t = resultFromStringBuilder;
+                }
+                #endregion
+
+                if (Regex.IsMatch(resultFromStringBuilder, commentPattern3))
+                {
+                    resultFromStringBuilder = Regex.Replace(resultFromStringBuilder, commentPattern3, matcher);
+                }
+
+                string[] resultFromStringBuilderArray = resultFromStringBuilder.Split('\n'); //split result to array for write in to file per string
+
+                ReadAndWriteTofilePerString.SaveStringinFilePerLineStrings(resultFromStringBuilderArray,
+                    fileNameToWrite);
+            }
+        }
+
+        private static string matcher(Match match)
+        {
+            if (!Regex.IsMatch(match.ToString(), @"[""].*?[""]"))
+            {
+                return "";
+            }
+            else
+            {
+                return match.Value;
+            }
         }
     }
 }
+// for /**/ comment ([\/][\*](\w|\P{L})*?[\*][\/])
+
+//string s = "\t";/**/*//*    /**//*
--- GSU-EPAM_Autumn_2016/ReadAndWriteTofilePerString.cs
+++ GSU-EPAM_Autumn_2016/ReadAndWriteTofilePerString.cs
@@ -5,84 +5,55 @@ using System.Text;
 using System.IO;
 
 namespace GSU_EPAM_Autumn_2016
-{
-    /// <summary>
-    /// Class for read and write per string
-    /// </summary>
+{ /// <summary>
+  /// Class for read and write per string
+  /// </summary>
     static class ReadAndWriteTofilePerString
     {
         /// <summary>
         /// Method for read file per string
         /// </summary>
         /// <param name="fileName">File name</param>
-        /// <param name="task">Task</param>
         /// <returns>File data in array</returns>
-        public static string[] LoadFileInStringPerLineStrings(string fileName, string task)
+        public static string[] LoadFileInStringPerLineStrings(string fileName)
         {
 
             #region find file in working directory
             string substr = "bin\\debug";
             string filePatch = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)
                  .ToLower().Replace(substr, "")
-                 + task + "\\" + fileName;
+                 + "\\" + fileName;
             #endregion
+
+
+
+            string[] fileDataPerLine = new string[0];
+
             #region read and write not null string from file
             try
             {
-                int count = 0;
-                #region Open file and count not null strings
-                try
+                //file open and count line.
+                using (StreamReader ins = new StreamReader(filePatch))
                 {
-                    //file open and count line.
-                    using (StreamReader ins = new StreamReader(filePatch))
+                    int index = 0;
+                    string fileData = null;
+                    while ((fileData = ins.ReadLine()) != null)
                     {
-                        string line = ins.ReadLine();//read line
-                        while (line != null)
-                        {
-                            if (line.Trim().Length > 0) //check line on empty
-                            {
-                                count++;
-                            }
-                            line = ins.ReadLine();
-                        }
+                        Array.Resize(ref fileDataPerLine, fileDataPerLine.Length + 1);
+                        fileDataPerLine[index] = fileData;
+                        index++;
+                        Console.WriteLine(fileData);
                     }
+
+
+                    fileData = ins.ReadToEnd();
                 }
-                catch (Exception)
-                {
-                    Console.WriteLine("s: File is not open for read");
-                }
-                #endregion
-                //generate output array based on conut not null strings in file
-                string[] fileDataPerLine = new string[count];
-                #region write all not null string from file in output array
-                try
-                {
-                    using (StreamReader ins = new StreamReader(filePatch))
-                    {
-                        string line = ins.ReadLine();
-                        count = 0;
-                        while (line != null)
-                        {
-                            line = line.Trim();//delete start end spases
-                            if (line.Length > 0)
-                            {
-                                fileDataPerLine[count++] = line;
-                            }
-                            line = ins.ReadLine();
-                        }
-                    }
-                }
-                catch (Exception)
-                {
-                    Console.WriteLine("File is not open for read and write to array");
-                    fileDataPerLine = null;
-                }
-                #endregion
                 return fileDataPerLine;
             }
-            catch (Exception)
+            catch (FileNotFoundException ex)
             {
-                Console.WriteLine("s: File is not open");
+                Console.WriteLine("FileNotFoundException :" + ex.Message);
+                Console.WriteLine("Trace :" + ex.StackTrace);
                 return null;
             }
             #endregion
@@ -90,16 +61,15 @@ namespace GSU_EPAM_Autumn_2016
         /// <summary>
         /// Method for write array in file per string
         /// </summary>
-        /// <param name="task">Task</param>
         /// <param name="inputStrings">Input array</param>
         /// <param name="fileNameToWrite">File name for write</param>
-        public static void SaveStringinFilePerLineStrings(string task, string[] inputStrings, string fileNameToWrite)
+        public static void SaveStringinFilePerLineStrings(string[] inputStrings, string fileNameToWrite)
         {
             #region find file in working directory
             string substr = "bin\\debug";
             string filePatch = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)
                  .ToLower().Replace(substr, "")
-                 + task + "\\" + fileNameToWrite;
+                 + "\\" + fileNameToWrite;
             #endregion
             #region write array in to file
             try
@@ -113,12 +83,12 @@ namespace GSU_EPAM_Autumn_2016
                     }
                 }
             }
-            catch (Exception)
+            catch (DirectoryNotFoundException ex)
             {
-                Console.WriteLine("File is not open for write");
+                Console.WriteLine("DirectoryNotFoundException :" + ex.Message);
+                Console.WriteLine("Trace :" + ex.StackTrace);
             }
             #endregion
         }
     }
 }
-
