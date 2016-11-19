2cada95ffe9dabcd26cf7bd526a2aab5d02e1caf
Taler <mrtaler@me.com>
2016-11-19 16:10:58 +0300

    гз

--- GSU-EPAM_Autumn_2016/Program.cs
+++ GSU-EPAM_Autumn_2016/Program.cs
@@ -10,6 +10,40 @@ namespace GSU_EPAM_Autumn_2016
     {
         static void Main(string[] args)
         {
+            #region Find file in working directory
+            string filePatch = args[0] + ".csv";
+        }
+            #endregion
+            try
+            {
+                using (StreamReader ins = new StreamReader(filePatch))
+                {
+                    purchaseList = new List<Purchase>();
+                    string fileDataPerString = null;
+                    while ((fileDataPerString = ins.ReadLine()) != null)
+                    {
+                        try
+                        {
+                            purchaseList.Add(createPurchase(fileDataPerString));
+                        }
+                        catch (CsvStringException ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                            if (ex.InnerException != null)
+                            {
+                                Console.WriteLine(ex.InnerException.Message);
+                                Console.WriteLine(ex.InnerException.TargetSite);
+                            }
+Console.WriteLine();
+                        }
+                    }
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("FileNotFoundException :" + ex.TargetSite);
+                purchaseList = new List<Purchase>();
+            }
         }
     }
 }
